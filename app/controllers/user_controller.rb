c7f51ca8b2a1db1f027597f9de1e5fed18c49632
rittersm <rittersm@gmail.com>
2016-09-05 19:14:21 -0400

    reformatted for heroku live

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,14 +2,14 @@ class UserController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_filter :newcontact
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     redirect_to root_path
+  #   else
+  #     redirect_to root_path
+  #   end
+  # end
 
   def show
     @blog = Blog.last
